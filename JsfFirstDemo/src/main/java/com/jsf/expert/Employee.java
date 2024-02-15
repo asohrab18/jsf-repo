c1eb6adc197e98d426ba8ceaa2502c28b479b714
Sohrab Alam <sohrab.alam86@gmail.com>
2024-02-15 21:22:40 +0530

    Enhanced radio buttons, checkboxes

--- JsfFirstDemo/src/main/java/com/jsf/expert/Employee.java
+++ JsfFirstDemo/src/main/java/com/jsf/expert/Employee.java
@@ -12,14 +12,23 @@ public class Employee {
 	private String lastName;
 	private String country;
 	private List<String> countryOptions;
+	private String gender;
+	private List<String> genderOptions;
+	private String[] favoriteLanguages;
 
 	public Employee() {
 		countryOptions = Arrays.asList("Australia", "Brazil", "China", "Denmark", "France", "Germany", "India", "Japan",
 				"Malaysia", "Netherlands");
+		genderOptions = Arrays.asList("Male", "Female", "Others");
+
 	}
 
 	public List<String> getCountryOptions() {
 		return countryOptions;
+	}
+
+	public List<String> getGenderOptions() {
+		return genderOptions;
 	}
 
 	public String getFirstName() {
@@ -44,6 +53,22 @@ public class Employee {
 
 	public void setCountry(String country) {
 		this.country = country;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String[] getFavoriteLanguages() {
+		return favoriteLanguages;
+	}
+
+	public void setFavoriteLanguages(String[] favoriteLanguages) {
+		this.favoriteLanguages = favoriteLanguages;
 	}
 
 }
