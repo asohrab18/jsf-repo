ee23606d169f868dbe0733b3c6e664db95284766
Sohrab Alam <sohrab.alam86@gmail.com>
2024-02-16 22:43:31 +0530

    Validation using Regex

---
+++ ValidationsDemo/src/main/java/com/jsf/expert/Person.java
@@ -0,0 +1,35 @@
+package com.jsf.expert;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Person {
+	
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
