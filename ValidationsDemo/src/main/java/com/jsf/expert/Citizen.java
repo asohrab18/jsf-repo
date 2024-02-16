977f4e5b89be3c145d79b1972c2bd31ccf7eb7be
Sohrab Alam <sohrab.alam86@gmail.com>
2024-02-16 22:18:56 +0530

    Validate Input Range & Length

---
+++ ValidationsDemo/src/main/java/com/jsf/expert/Citizen.java
@@ -0,0 +1,44 @@
+package com.jsf.expert;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Citizen {
+	
+	private String firstName;
+	private String lastName;
+	private String postalCode;
+	private Integer freePasses;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+}
