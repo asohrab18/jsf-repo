a189dfac74aa1b1502e93b17fbc6cd37914c00a9
Sohrab Alam <sohrab.alam86@gmail.com>
2024-02-16 21:39:18 +0530

    Field level error messages by validations

---
+++ ValidationsDemo/src/main/java/com/jsf/expert/Employee.java
@@ -0,0 +1,35 @@
+package com.jsf.expert;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Employee {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
